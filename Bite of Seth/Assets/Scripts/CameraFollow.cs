1671673022d54076b7018a889bc9c8198dff770b
GeorgeManeta <georgemaneta@gmail.com>
2020-03-31 01:34:09 -0300

    Player movement and camera follow

---
+++ Bite of Seth/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 offset;
+
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+        offset.x = 0; 
+        offset.y = 0;
+    }
+
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
