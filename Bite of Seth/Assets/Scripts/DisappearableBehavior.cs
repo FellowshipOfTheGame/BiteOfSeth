df12273e35cc752d8db7585473f908a93f9fd2da
Mateus Prado <mateus.prado@usp.br>
2020-04-25 21:38:50 -0300

    DisappearableBase

---
+++ Bite of Seth/Assets/Scripts/DisappearableBehavior.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisappearableBehavior : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("jooj");
+    }
+
+    //Disappears when the player exits this gameObject
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (LayerMask.LayerToName(collision.gameObject.layer) == "Player") {
+            Destroy(gameObject);
+        }
+    }
+
+
+}
