8ef3a9e2ddccace27991b875b3a373364ac4b661
Mateus Prado <mateus.prado@usp.br>
2020-04-27 17:00:03 -0300

    Base is disappearing

--- Bite of Seth/Assets/Scripts/DisappearableBehavior.cs
+++ Bite of Seth/Assets/Scripts/DisappearableBehavior.cs
@@ -5,16 +5,12 @@ using UnityEngine;
 public class DisappearableBehavior : MonoBehaviour
 {
 
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        Debug.Log("jooj");
-    }
-
     //Disappears when the player exits this gameObject
     private void OnTriggerExit2D(Collider2D collision)
     {
+
         if (LayerMask.LayerToName(collision.gameObject.layer) == "Player") {
-            Destroy(gameObject);
+            Destroy(gameObject.transform.parent.gameObject);
         }
     }
 
