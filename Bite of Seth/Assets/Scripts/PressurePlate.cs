365b896f9b703457347d7d4a0e5a4bcf8de9ed10
GeorgeManeta <georgemaneta@gmail.com>
2020-04-27 02:04:44 -0300

    pressure plate and door working

---
+++ Bite of Seth/Assets/Scripts/PressurePlate.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressurePlate : MonoBehaviour
+{
+    public LayerMask pressMask = default;
+    public bool isPressed = false;
+
+    private void FixedUpdate()
+    {
+        isPressed = GridNav.GetObjectsInPath(gameObject.transform.position, GridNav.up, pressMask, gameObject).Count > 0;
+    }
+}
