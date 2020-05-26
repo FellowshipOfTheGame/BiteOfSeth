e25a06f73443be93dc475d016fb0ac0eeacf8c2f
LeodeWill <leode.g.will@gmail.com>
2020-05-26 18:02:58 -0300

    Better Ground Tile

--- Bite of Seth/Assets/Scripts/TilemapScripts/RuleTile.cs
+++ Bite of Seth/Assets/Scripts/TilemapScripts/RuleTile.cs
@@ -6,37 +6,53 @@ using UnityEngine.Tilemaps;
 [CreateAssetMenu]
 public class RuleTile : Tile {
     //[HideInInspector] public bool updated = false;
-    public Sprite middle;
-    public Sprite[] oneSide;
-    public Sprite[] twoSide;
+    public Sprite[] main, upGnd, downGnd, leftGnd, rightGnd;
+    public Sprite[] vert, hor, upLeft, upRight, downRight, downLeft;
+    public Sprite[] noOpen, upOpen, downOpen, leftOpen, rightOpen;
 
-    public bool[,] neighbours;
 
     public override void RefreshTile(Vector3Int position, ITilemap tilemap) {
         for (int yd = -1; yd <= 1; yd++){
-            for (int xd = -1; xd <= 1; xd++){
-                Vector3Int location = new Vector3Int(position.x + xd, position.y + yd, position.z);
-                if (IsNeighbour(location, tilemap))   
-                    tilemap.RefreshTile(location);  
-            }
+            Vector3Int location = new Vector3Int(position.x, position.y + yd, position.z);
+            if (IsNeighbour(location, tilemap))   
+                tilemap.RefreshTile(location);  
+        }
+
+        for (int xd = -1; xd <= 1; xd++){
+            Vector3Int location = new Vector3Int(position.x + xd, position.y, position.z);
+            if (IsNeighbour(location, tilemap))   
+                tilemap.RefreshTile(location);  
         }
     }
 
     public override void GetTileData(Vector3Int position, ITilemap tilemap, ref TileData tileData){
-        neighbours = new bool[3,3];
+        bool[] neighbours = new bool[4];
         int count = 0;
 
-        for (int yd = -1; yd <= 1; yd++){
-            for (int xd = -1; xd <= 1; xd++){
-                if (xd != 0 || yd != 0){
-                    Vector3Int location = new Vector3Int(position.x + xd, position.y + yd, position.z);
-                    if (IsNeighbour(location, tilemap)){
-                        this.neighbours[1+xd,1-yd] = true;
-                        count++;
-                    }
-                }     
-            }
+        Vector3Int location = new Vector3Int(position.x, position.y + 1, position.z);
+        if (IsNeighbour(location, tilemap)){
+            neighbours[0] = true;
+            count++;
         }
+
+        location = new Vector3Int(position.x, position.y - 1, position.z);
+        if (IsNeighbour(location, tilemap)){
+            neighbours[1] = true;
+            count++;
+        }
+
+        location = new Vector3Int(position.x - 1, position.y, position.z);
+        if (IsNeighbour(location, tilemap)){
+            neighbours[2] = true;
+            count++;
+        }
+
+        location = new Vector3Int(position.x + 1, position.y, position.z);
+        if (IsNeighbour(location, tilemap)){
+            neighbours[3] = true;
+            count++;
+        }
+        
         
         tileData.sprite = calcSprite(neighbours, count);  
     }
@@ -46,9 +62,38 @@ public class RuleTile : Tile {
         return (tile != null && tile == this);
     }
   
-    Sprite calcSprite(bool[,] neighbours, int count){
-        if (!neighbours[1,0]) return oneSide[0];
-        
-        return middle;
+    Sprite calcSprite(bool[] neighbours, int count){
+        switch (count){
+            case 0:
+                return noOpen[0];
+
+            case 1:
+                if (neighbours[0]) return upOpen[0];
+                if (neighbours[1]) return downOpen[0];
+                if (neighbours[2]) return leftOpen[0];
+                return rightOpen[0];
+            
+            case 2:
+                if(neighbours[0]){
+                    if (neighbours[1]) return vert[0];
+                    if (neighbours[2]) return upLeft[0];
+                    return upRight[0];
+                }
+                if(neighbours[1]){
+                    if (neighbours[2]) return downLeft[0];
+                    return downRight[0];
+                }
+                return hor[0];
+
+            case 3:
+                if (!neighbours[0]) return upGnd[0];
+                if (!neighbours[1]) return downGnd[0];
+                if (!neighbours[2]) return leftGnd[0];
+                return rightGnd[0];
+
+            
+            default:
+                return main[0];
+        }
     }
 }
