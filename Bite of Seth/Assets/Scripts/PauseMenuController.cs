4f83584ea69fe9dc0ec769a27c71c3430ba3efe5
GeorgeManeta <georgemaneta@gmail.com>
2020-05-26 23:26:55 -0300

    SceneObjectPack created

--- Bite of Seth/Assets/Scripts/PauseMenuController.cs
+++ Bite of Seth/Assets/Scripts/PauseMenuController.cs
@@ -4,9 +4,19 @@ using UnityEngine;
 
 public class PauseMenuController : MonoBehaviour
 {
+    public KeyCode openMenuKey = KeyCode.None;
     public GameObject canvas = null;
     public GameObject mainLayout = null;
     public GameObject instructionLayout = null;
+
+    public void Update()
+    {
+        // fazer aqui os checks q impedem o menu de abrir
+        if (Input.GetKeyDown(openMenuKey) && canvas.activeSelf == false)
+        {
+            OpenMenu();
+        }
+    }
 
     public void OpenMenu()
     {
