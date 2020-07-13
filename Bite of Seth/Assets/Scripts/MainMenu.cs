831a18819686ba8270cd09fa104a8b7f85501f06
Marcelo Temoteo <marcelo.temoteo@hotmail.com>
2020-07-13 17:04:05 -0300

    Main Menu SHOULD be functional if scenes are in the build
    Main menu should be functional, aside from the credits button (because we still don't have a credits screen).

---
+++ Bite of Seth/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public SceneReference FirstScene;
+
+    public void StartGame(){
+        SceneManager.LoadScene(FirstScene);
+    }
+
+    public void QuitGame(){
+        Debug.Log("Quit button pressed.");
+        Application.Quit();
+    }
+}
