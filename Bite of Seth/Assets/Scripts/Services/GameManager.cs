6e3018025215de06712a9d375c5095171037b218
GeorgeManeta <georgemaneta@gmail.com>
2020-08-22 15:30:56 -0300

    mouse desabilitado na navegação da UI

--- Bite of Seth/Assets/Scripts/Services/GameManager.cs
+++ Bite of Seth/Assets/Scripts/Services/GameManager.cs
@@ -25,9 +25,10 @@ public class GameManager : GameService
 
     public override void Start()
     {
+        base.Start();
+
         score = 0;
 
-        base.Start();
         lockMovement = false;
 
         levelData = new List<LevelData>();
