dc3e8c5377555479bd5b13e8e2ae35b67f64857e
leodewill <leode.g.will@gmail.com>
2020-09-25 15:57:26 -0300

    Smooth Menu Tab Switch

--- Bite of Seth/Assets/Animation/Menu/MaterialController.cs
+++ Bite of Seth/Assets/Animation/Menu/MaterialController.cs
@@ -7,14 +7,31 @@ public class MaterialController : MonoBehaviour {
     public Material material;
     public Vector2 speed;
 
+    public bool auto = true;
+
+    bool move = false;
+
     // Start is called before the first frame update
     void Start(){
-        
+        move = false;
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        material.mainTextureOffset += speed * Time.deltaTime;
+    void Update(){
+        if (auto || move) {
+            material.mainTextureOffset += speed * Time.deltaTime;
+        }
+    }
+
+    public void TurnAndMove(float time) {
+        speed = -speed;
+        move = true;
+
+        CancelInvoke();
+        Invoke("Stop", time);
+    }
+
+    void Stop() {
+        move = false;
     }
 }
