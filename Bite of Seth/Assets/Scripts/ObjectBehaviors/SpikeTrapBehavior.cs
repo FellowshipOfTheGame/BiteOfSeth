890f1d39069011d147a7a62d0fefa845d6960c22
Mateus Prado <mateus.prado@usp.br>
2020-10-23 21:11:08 -0300

    Spike Trap pt.2

--- Bite of Seth/Assets/Scripts/ObjectBehaviors/SpikeTrapBehavior.cs
+++ Bite of Seth/Assets/Scripts/ObjectBehaviors/SpikeTrapBehavior.cs
@@ -10,8 +10,7 @@ public class SpikeTrapBehavior : MonoBehaviour
 
     private float timeCounter = 0f;
     private bool activated = false;
-
-    private DamageOnTouchBehavior dot;
+    
     private BoxCollider2D bc;
     public SpriteRenderer sr;
 
@@ -20,8 +19,7 @@ public class SpikeTrapBehavior : MonoBehaviour
     {
         bc = GetComponent<BoxCollider2D>();
         bc.enabled = false;
-        dot = GetComponent<DamageOnTouchBehavior>();
-        dot.enabled = false;
+        sr.color = Color.gray;
     }
 
     // Update is called once per frame
@@ -29,34 +27,46 @@ public class SpikeTrapBehavior : MonoBehaviour
     {
         timeCounter += Time.fixedDeltaTime;
 
-        if (activated) {
-            Vector2 dir = new Vector2(0.01f, 0.01f);
-            List<GameObject> objects = GridNav.GetObjectsInPath(transform.position, dir, gameObject);
-            foreach (GameObject g in objects) {
-                //Try to kill player
-                if (g.layer == LayerMask.NameToLayer("Player")) {
-                    // kill player
-                    ServiceLocator.Get<GameManager>().KillPlayer();
-                } else {
-                    // kill other entities here
-                }
-            }
-        }
-
+        //Control the trap activation and deactivation timers 
         if (!activated && timeCounter >= deactivatedTimer) {
-            activated = true;
-            bc.enabled = true;
-            timeCounter = 0;
-            dot.enabled = true;
-            sr.color = Color.red;
+            ActivateTrap();
         } else if(activated && timeCounter >= activatedTimer) {
-            activated = false;
-            bc.enabled = false;
-            timeCounter = 0;
-            dot.enabled = false;
-            sr.color = Color.white;
+            DeactivateTrap();
         }
         
+    }
+
+    private void ActivateTrap()
+    {
+        activated = true;
+        bc.enabled = true;
+        sr.color = Color.red;
+        ResetCounter();
+    }
+
+    private void DeactivateTrap()
+    {
+        activated = false;
+        bc.enabled = false;
+        sr.color = Color.gray;
+        ResetCounter();
+    }
+
+    private void ResetCounter()
+    {
+        timeCounter = 0;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.collider.gameObject.layer == LayerMask.NameToLayer("Player")) {
+            ServiceLocator.Get<GameManager>().KillPlayer();
+        } else {
+            //Se for qualquer outro objeto que se move(pedra, por exemplo)
+            //então desativa o colisor
+            bc.enabled = false;
+            sr.color = Color.gray;
+        }
     }
 
 }
