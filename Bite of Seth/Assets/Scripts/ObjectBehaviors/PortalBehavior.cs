048c7db547cce8add0b56292b0ea75e765cc305d
Mateus Prado <mateus.prado@usp.br>
2020-10-31 16:46:17 -0300

    Logic World pt. 2

--- Bite of Seth/Assets/Scripts/ObjectBehaviors/PortalBehavior.cs
+++ Bite of Seth/Assets/Scripts/ObjectBehaviors/PortalBehavior.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class PortalBehavior : MonoBehaviour
 {
+
+    public GameObject otherSidePortalRef;
+    private bool playerInRange;
+    private GameObject playerRef;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,30 @@ public class PortalBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.T) && playerInRange) {
+            playerRef.transform.position = otherSidePortalRef.transform.position;
+        }
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if (collision.CompareTag("Player")) {
+            playerRef = collision.gameObject;
+            playerInRange = true;
+        }
+
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+
+        if (collision.CompareTag("Player")) {
+            playerRef = null;
+            playerInRange = false;
+        }
+    }
+
+
+
 }
