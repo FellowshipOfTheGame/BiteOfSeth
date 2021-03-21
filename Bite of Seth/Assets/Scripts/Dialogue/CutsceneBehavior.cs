094c15e2079b66f6dad13e5667de11300beb474b
leodewill <leode.g.will@gmail.com>
2021-03-21 15:18:41 -0300

    BASICS OF CUTSCENE

---
+++ Bite of Seth/Assets/Scripts/Dialogue/CutsceneBehavior.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class CutsceneBehavior : DialogueBehavior {
+
+    public PlayableDirector timeline;
+
+    public override void OnDialog() {
+        //base.OnDialog();
+        ts.playerInRange = true;
+        timeline.playableGraph.GetRootPlayable(0).SetSpeed(0);
+    }
+
+    public override void OnEndDialog() {
+        //base.OnEndDialog();
+        ts.playerInRange = false;
+        timeline.playableGraph.GetRootPlayable(0).SetSpeed(1);
+    }
+}
