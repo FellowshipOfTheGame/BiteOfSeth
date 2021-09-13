dda285fe9fde480ab22494cc91d8858ba89caed4
Mateus Prado <mateus.prado@usp.br>
2021-09-12 21:48:34 -0300

    One Way Tile

---
+++ Bite of Seth/Assets/Scripts/ChangeToWall.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeToWall : MonoBehaviour
+{
+
+    public GameObject defaultSprite;
+    public GameObject wallSprite;
+
+    public void Change()
+    {
+        gameObject.layer = 9; //change layer to Wall layer
+        defaultSprite.SetActive(false);
+        wallSprite.SetActive(true);
+        GetComponent<BoxCollider2D>().enabled = true;
+    }
+
+
+}
