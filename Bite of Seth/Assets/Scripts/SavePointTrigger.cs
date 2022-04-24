5b525642c8aa7b7ad51d992bd1f0f7de47da3259
Mateus Prado <mateuspradotimao@gmail.com>
2022-04-24 13:28:27 -0300

    Level 3 Puzzles pt. 4

---
+++ Bite of Seth/Assets/Scripts/SavePointTrigger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SavePointTrigger : MonoBehaviour
+{
+    public GameObject savePoint;
+
+    public void TriggerActiveTrue()
+    {
+        savePoint.SetActive(true);
+    }
+
+    public void TriggerActiveFalse()
+    {
+        savePoint.SetActive(false);
+    }
+
+}
